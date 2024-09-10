fcef7ffd537a88d2d720afdfcc8b5fdc17cbc144
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-09-10 18:04:12 +0900

    chore:socket 설정 및 port 3000으로 변경

--- dns_front/src/main.js
+++ dns_front/src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import io from "socket.io-client";
-const socket = io("http://localhost:8080");
 
+//소켓 관련 설정
+const socket = io("http://localhost:8080", {
+  transports: ["websocket"],
+  withCredentials: true,
+});
+//전역으로 소켓을 사용할 수 있게 등록
 Vue.prototype.$socket = socket;
 
 //vuetify 스타일 추가
--- dns_front/vue.config.js
+++ dns_front/vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+  //스프링 부트와 겹치기 때문에 3000 포트로 변경
+  devServer: {
+    port: 3000,
+  },
+});
