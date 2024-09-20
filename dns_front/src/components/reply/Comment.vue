1b05724b414216cdea89c8cbdaac75389c7b72f7
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-09-20 18:28:10 +0900

    feat: 댓글 및 대댓글 조회 구현

---
+++ dns_front/src/components/reply/Comment.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <!-- 수정 상태가 false -->
+    <ul v v-if="isComment == false">
+      <div>
+        댓글번호 : {{ comment.commentId }}
+        {{ comment.memberProfileImage }}
+        {{ comment.memberProfile.nickname }}<br />
+        {{ comment.commentContent }}
+        <button @click="editComment(comment.commentId)">수정</button>
+        <button @click="deleteComment(post.postId, comment.commentId)">
+          삭제
+        </button>
+      </div>
+    </ul>
+    <!-- 수정 상태가 true -->
+    <ul v v-else>
+      <div>
+        {{ comment.memberProfileImage }}
+        {{ comment.memberNickName }}<br />
+        <input type="text" v-model="editCommentText" />
+        <button @click="EDIT_Comment()">수정완료</button>
+        <button @click="editComment()">취소</button>
+      </div>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+    comment: {
+      //       "content": [
+      // {
+      // "commentId": 12,
+      // "postId": 2,
+      // "memberProfile": {
+      // "memberId": 1,
+      // "profileImageUrl": "a"
+      // "nickname": "a"
+      // },
+      // "commentContent": "12",
+      // "createdAt": "2024-09-18T16:02:54"
+      // "updatedAt": null,
+      // "parentCommentId": null,
+      // "deleted": false
+      // ] ,
+      // "pageNumber": 0,
+      // "size": 10,
+      // "first": true,
+      // "last": true
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      editCommentText: "",
+      isComment: false,
+    };
+  },
+  methods: {
+    //댓글 수정 상태 true <> false
+    editComment() {
+      this.isComment = !this.isComment;
+      this.editCommentText = this.comment.commentContent;
+    },
+
+    editCommentCancel() {
+      this.isComment = false;
+      this.editCommentText = "";
+    },
+    //댓글 수정
+    EDIT_Comment() {
+      // this.$axios
+      //   .patch(
+      //     `/post/${this.post.postId}/comments/${this.comment.commentId}`,
+      //     this.editCommentText
+      //   )
+      //   .then((res) => {
+      //     if (res.state == 202) {
+      alert("수정이 완료되었습니다.");
+      this.isComment = false;
+      //   } else {
+      //     alert("실패했습니다. 다시 시도해주세요.");
+      //     this.isComment = false;
+      //   }
+      // });
+    },
+    //댓글 삭제
+    deleteComment(postId, commentId) {
+      //로그인한 멤버id랑 비교하는거로 수정해야함
+      if (this.comments.memberId == 1) {
+        this.$axios
+          .delete(`/post/${postId}/comments/${commentId}`)
+          .then((res) => {
+            if (res.status == 204) {
+              alert("삭제가 완료되었습니다.");
+            } else {
+              alert("실패했습니다. 다시 시도해주세요.");
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+    },
+  },
+};
+</script>
--- dns_front/src/components/reply/reply.vue
+++ dns_front/src/components/reply/reply.vue
@@ -1,90 +1,18 @@
 <template>
   <div>
-    <!-- 수정 상태가 false -->
-    <ul v v-if="isReply == false">
-      <div>
-        {{ reply.profile }}
-        {{ reply.nickName }}<br />
-        {{ reply.comment }}
-        <button @click="editReply(reply.commnetId)">수정</button>
-        <button @click="deleteReply(post.postId, reply.commnetId)">삭제</button>
-      </div>
-    </ul>
-    <!-- 수정 상태가 true -->
-    <ul v v-else>
-      <div>
-        {{ reply.profile }}
-        {{ reply.nickName }}<br />
-        <input type="text" v-model="editReplyText" />
-        <button @click="EDIT_REPLY()">수정완료</button>
-        <button @click="editReply()">취소</button>
-      </div>
-    </ul>
+    프로필 이미지 :{{ reply.memberProfileImage }} 닉네임 :{{
+      reply.memberNickName
+    }}
+    작성 내용 : {{ reply.commentContent }} <br />부모댓글 번호 :
+    {{ reply.parentCommentId }}
   </div>
 </template>
 <script>
 export default {
   props: {
-    post: {
-      type: Array,
-      required: true,
-    },
     reply: {
       type: Object,
       required: true,
-    },
-  },
-  data() {
-    return {
-      editReplyText: "",
-      isReply: false,
-    };
-  },
-  methods: {
-    //댓글 수정 상태 true <> false
-    editReply() {
-      this.isReply = !this.isReply;
-      this.editReplyText = this.reply.comment;
-      console.log(this.reply);
-    },
-    editReplyCancel() {
-      this.isReply = false;
-      this.editReplyText = "";
-    },
-    //댓글 수정
-    EDIT_REPLY() {
-      // this.$axios
-      //   .patch(
-      //     `/post/${this.post.postId}/comments/${this.reply.commentId}`,
-      //     this.editReplyText
-      //   )
-      //   .then((res) => {
-      //     if (res.state == 202) {
-      alert("수정이 완료되었습니다.");
-      this.isReply = false;
-      //   } else {
-      //     alert("실패했습니다. 다시 시도해주세요.");
-      //     this.isReply = false;
-      //   }
-      // });
-    },
-    //댓글 삭제
-    deleteReply(postId, commentId) {
-      //로그인한 멤버id랑 비교하는거로 수정해야함
-      if (this.comments.memberId == 1) {
-        this.$axios
-          .delete(`/post/${postId}/comments/${commentId}`)
-          .then((res) => {
-            if (res.status == 204) {
-              alert("삭제가 완료되었습니다.");
-            } else {
-              alert("실패했습니다. 다시 시도해주세요.");
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      }
     },
   },
 };
