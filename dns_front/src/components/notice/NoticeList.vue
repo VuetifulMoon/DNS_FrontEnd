055bb5c9ec3501ca64214810c007a53752635b02
dyddks <dyddks0115@naver.com>
2024-09-30 12:35:30 +0900

    feat: 알림기능 구현

---
+++ dns_front/src/components/notice/NoticeList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NoticeItem/>
+  </div>
+</template>
+
+<script>
+import NoticeItem from './NoticeItem.vue';
+
+export default {
+  components: {
+    NoticeItem
+  },
+  created() {
+    this.$store.dispatch('fetchNotice', 1);
+  }
+}
+</script>
+
+<style>
+
+</style>
