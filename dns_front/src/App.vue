c323a7535f872ed5da31e8445344f4dae4140165
canelo <chyg201672@gmail.com>
2024-10-08 18:02:05 +0900

    style: app.vue 스타일 추가

--- dns_front/src/App.vue
+++ dns_front/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <Router-link to="/">Home</Router-link>
-    <br />
-    <Router-link to="/dmMain">DM</Router-link>
-    <br />
-    <Router-link to="/posts">Board</Router-link>
-    <br />
-    <Router-link to="/notification/1">알림</Router-link>
-    <br />
-    <Router-link to="/memberSignup">회원가입</Router-link>
+  <div class="home">
+    <div class="navBar">
+      <Router-link to="/">Home</Router-link>
+      <br />
+      <Router-link to="/dmMain">DM</Router-link>
+      <br />
+      <Router-link to="/board">Board</Router-link>
+      <br />
+      <Router-link to="/notification/1">알림</Router-link>
+      <br />
+      <Router-link to="/memberSignup">회원가입</Router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -23,4 +25,14 @@ export default {
 };
 </script>
 <style>
+.home {
+  display: flex;
+  display: 100%;
+}
+.navBar {
+  display: flex;
+  flex-direction: column;
+  height: 1080px;
+  background-color: aqua;
+}
 </style>
